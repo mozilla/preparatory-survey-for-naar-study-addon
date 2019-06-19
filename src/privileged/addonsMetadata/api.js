f28e2ea35f81abd4fd4e4d1c66a17a3feed2350d
Fredrik Wollsén <fredrik@neam.se>
2019-06-19 15:59:46 +0300

    Include icon url info in list of installed addons

--- src/privileged/addonsMetadata/api.js
+++ src/privileged/addonsMetadata/api.js
@@ -10,11 +10,37 @@ this.addonsMetadata = class extends ExtensionAPI {
     const { TelemetryEnvironment } = ChromeUtils.import(
       "resource://gre/modules/TelemetryEnvironment.jsm",
     );
+    const { AddonManager } = ChromeUtils.import(
+      "resource://gre/modules/AddonManager.jsm",
+    );
     return {
       addonsMetadata: {
         async getListOfInstalledAddons() {
           await TelemetryEnvironment.onInitialized();
-          return TelemetryEnvironment.currentEnvironment.addons;
+
+          // Get general addon metadata from the already prepared addons information in the telemetry environment
+          const telAddons =
+            TelemetryEnvironment.currentEnvironment.addons.activeAddons;
+
+          // Get icon urls
+          const activeAddons = await AddonManager.getActiveAddons();
+          const addonsIconUrlInfos = activeAddons.addons.map(addon => {
+            const id = addon.id;
+            const iconUrl =
+              AddonManager.getPreferredIconURL(addon, 256) || false;
+            return { id, iconUrl };
+          });
+
+          // Return general addon metadata merged with the icon url info
+          return Object.keys(telAddons).map(addonId => {
+            const iconUrlInfo = addonsIconUrlInfos.find(
+              $iconUrlInfo => ($iconUrlInfo.id = addonId),
+            );
+            return {
+              ...telAddons[addonId],
+              iconUrl: iconUrlInfo.iconUrl,
+            };
+          });
         },
       },
     };
