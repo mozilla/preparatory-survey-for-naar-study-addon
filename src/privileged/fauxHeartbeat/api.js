fb8efde77329738617920f411354d5d8182fb256
Fredrik Wollsén <fredrik@neam.se>
2019-06-20 11:27:24 +0300

    Faux heartbeat works with latest version of fx

--- src/privileged/fauxHeartbeat/api.js
+++ src/privileged/fauxHeartbeat/api.js
@@ -3,7 +3,6 @@
 /* global ExtensionAPI */
 
 ChromeUtils.import("resource://gre/modules/Console.jsm");
-ChromeUtils.import("resource://gre/modules/Services.jsm");
 ChromeUtils.import("resource://gre/modules/XPCOMUtils.jsm");
 ChromeUtils.import("resource://gre/modules/ExtensionCommon.jsm");
 ChromeUtils.import("resource://gre/modules/ExtensionUtils.jsm");
@@ -12,89 +11,6 @@ ChromeUtils.import("resource://gre/modules/ExtensionUtils.jsm");
 const { EventManager } = ExtensionCommon;
 const EventEmitter =
   ExtensionCommon.EventEmitter || ExtensionUtils.EventEmitter;
-
-// eslint-disable-next-line no-undef
-XPCOMUtils.defineLazyModuleGetter(
-  this,
-  "BrowserWindowTracker",
-  "resource:///modules/BrowserWindowTracker.jsm",
-);
-
-let notificationBox = null;
-let notice = null;
-
-// Style the notification like Heartbeat (selectively copied from Heartbeat.jsm)
-const HEARTBEAT_CSS_URI = Services.io.newURI(
-  "resource://normandy/skin/shared/Heartbeat.css",
-);
-const HEARTBEAT_CSS_URI_OSX = Services.io.newURI(
-  "resource://normandy/skin/osx/Heartbeat.css",
-);
-const windowsWithInjectedCss = new WeakSet();
-let anyWindowsWithInjectedCss = false;
-
-function removeActiveNotification() {
-  if (notice && notificationBox) {
-    try {
-      notificationBox.removeNotification(notice);
-    } catch (err) {
-      // The dom nodes are probably gone. That's fine.
-    }
-  }
-}
-
-function showNotification(
-  chromeWindow,
-  notificationMessage,
-  buttonLabel,
-  onClickButton,
-  onDismissed,
-) {
-  removeActiveNotification();
-
-  notificationBox = chromeWindow.gHighPriorityNotificationBox;
-  notice = notificationBox.appendNotification(
-    notificationMessage,
-    "naar-survey-prompt-1",
-    "resource://normandy/skin/shared/heartbeat-icon.svg",
-    notificationBox.PRIORITY_INFO_HIGH,
-    [
-      {
-        label: buttonLabel,
-        callback: onClickButton,
-      },
-    ],
-    eventType => {
-      if (eventType === "dismissed") {
-        onDismissed();
-      }
-    },
-  );
-
-  // Style the notification like Heartbeat (selectively copied from Heartbeat.jsm)
-  if (!windowsWithInjectedCss.has(chromeWindow)) {
-    windowsWithInjectedCss.add(chromeWindow);
-    const utils = chromeWindow.windowUtils;
-    utils.loadSheet(HEARTBEAT_CSS_URI, chromeWindow.AGENT_SHEET);
-    if (AppConstants.platform === "macosx") {
-      utils.loadSheet(HEARTBEAT_CSS_URI_OSX, chromeWindow.AGENT_SHEET);
-    }
-    anyWindowsWithInjectedCss = true;
-  }
-  notice.messageDetails.style.overflow = "hidden";
-  notice.messageImage.classList.add("heartbeat", "pulse-onshow");
-  notice.messageText.classList.add("heartbeat");
-  notice.messageText.flex = 0;
-  notice.spacer.flex = 0;
-  notice.classList.add("heartbeat");
-}
-
-function getMostRecentNonPrivateBrowserWindow() {
-  return BrowserWindowTracker.getTopWindow({
-    private: false,
-    allowPopups: false,
-  });
-}
 
 /**
  * Display notification bar
@@ -107,6 +23,92 @@ class FauxHeartbeatEventEmitter extends EventEmitter {}
 
 this.fauxHeartbeat = class extends ExtensionAPI {
   getAPI(context) {
+    const { Services } = ChromeUtils.import(
+      "resource://gre/modules/Services.jsm",
+      {},
+    );
+
+    const { BrowserWindowTracker } = ChromeUtils.import(
+      "resource:///modules/BrowserWindowTracker.jsm",
+      {},
+    );
+
+    function removeActiveNotification() {
+      if (notice && notificationBox) {
+        try {
+          notificationBox.removeNotification(notice);
+        } catch (err) {
+          // The dom nodes are probably gone. That's fine.
+        }
+      }
+    }
+
+    function showNotification(
+      chromeWindow,
+      notificationMessage,
+      buttonLabel,
+      onClickButton,
+      onDismissed,
+    ) {
+      removeActiveNotification();
+
+      notificationBox = chromeWindow.gHighPriorityNotificationBox;
+      notice = notificationBox.appendNotification(
+        notificationMessage,
+        "naar-survey-prompt-1",
+        "resource://normandy/skin/shared/heartbeat-icon.svg",
+        notificationBox.PRIORITY_INFO_HIGH,
+        [
+          {
+            label: buttonLabel,
+            callback: onClickButton,
+          },
+        ],
+        eventType => {
+          if (eventType === "dismissed") {
+            onDismissed();
+          }
+        },
+      );
+
+      // Style the notification like Heartbeat (selectively copied from Heartbeat.jsm)
+      if (!windowsWithInjectedCss.has(chromeWindow)) {
+        windowsWithInjectedCss.add(chromeWindow);
+        const utils = chromeWindow.windowUtils;
+        utils.loadSheet(HEARTBEAT_CSS_URI, chromeWindow.AGENT_SHEET);
+        if (AppConstants.platform === "macosx") {
+          utils.loadSheet(HEARTBEAT_CSS_URI_OSX, chromeWindow.AGENT_SHEET);
+        }
+        anyWindowsWithInjectedCss = true;
+      }
+      notice.messageDetails.style.overflow = "hidden";
+      notice.messageImage.classList.add("heartbeat", "pulse-onshow");
+      notice.messageText.classList.add("heartbeat");
+      notice.messageText.flex = 0;
+      notice.spacer.flex = 0;
+      notice.classList.add("heartbeat");
+    }
+
+    function getMostRecentNonPrivateBrowserWindow() {
+      return BrowserWindowTracker.getTopWindow({
+        private: false,
+        allowPopups: false,
+      });
+    }
+
+    let notificationBox = null;
+    let notice = null;
+
+    // Style the notification like Heartbeat (selectively copied from Heartbeat.jsm)
+    const HEARTBEAT_CSS_URI = Services.io.newURI(
+      "resource://normandy/skin/shared/Heartbeat.css",
+    );
+    const HEARTBEAT_CSS_URI_OSX = Services.io.newURI(
+      "resource://normandy/skin/osx/Heartbeat.css",
+    );
+    const windowsWithInjectedCss = new WeakSet();
+    let anyWindowsWithInjectedCss = false;
+
     const fauxHeartbeatEventEmitter = new FauxHeartbeatEventEmitter();
     return {
       fauxHeartbeat: {
